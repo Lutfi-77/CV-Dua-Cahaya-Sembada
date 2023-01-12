245f948445f67b92ca87e5681bc573f1cc427aa6
Lutfi-77 <xenonoffic@gmail.com>
2023-01-13 03:51:45 +0700

    update dashboard

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -5,14 +5,28 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+use App\Models\Service;
+use App\Models\Project;
+use App\Models\Client;
+
 class DashboardController extends Controller
 {
     //
 
     public function index()
     {
+        $client = Client::count();
+        $category = Category::count();
+        $project = Project::count();
+        $service = Service::count();
+
         $data = [
-            'menus' => $this->menus
+            'menus' => $this->menus,
+            'total_client' => $client,
+            'total_category' => $category,
+            'total_project' => $project,
+            'total_service' => $service
         ];
         
         return view('admin.dashboard', $data);
